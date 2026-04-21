04659f16ddb03e2b1790087759f6bd7205e9ccfb
AfonsoSantos8 <afonso.santos172004@gmail.com>
2026-04-21 19:32:15 +0100

    urls invoice, refuel

--- Backend/project/urls.py
+++ Backend/project/urls.py
@@ -6,6 +6,8 @@ urlpatterns = [
     path('api/taxis/', include('apps.taxi.urls')),
     path('api/trip/', include('apps.trip.urls')),
     path('api/turnos/', include('apps.shift.urls')),
+    path('api/invoice/', include('apps.invoice.urls')),
+    path('api/refuel/', include('apps.refuel.urls')),
     path('api/clientes/', include('apps.user.client.urls')),
     path('auth/', include('apps.user.auth_urls')),
 ]
