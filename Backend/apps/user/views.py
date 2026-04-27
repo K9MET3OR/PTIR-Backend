ddf0380aecd2cc0a0a83f410635ecfae1cc01acf
Francisco Gomes <francisco.gomes.030505@gmail.com>
2026-04-27 17:17:15 +0100

    update validação nif

--- Backend/apps/user/views.py
+++ Backend/apps/user/views.py
@@ -1,6 +1,7 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from functools import wraps
+from django.db import connection
 
 from .models import User
 
