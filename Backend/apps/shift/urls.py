8d3c640a54b9c0a5991bee5aac899d6ebfa41441
Francisco Gomes <francisco.gomes.030505@gmail.com>
2026-04-27 17:49:04 +0100

    turno

--- Backend/apps/shift/urls.py
+++ Backend/apps/shift/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('taxis-disponiveis/', views.taxis_disponiveis),
+    path('registar/', views.registar_shift),
+    path('driver/<int:driver_id>/', views.listar_shifts_driver),
+    path('<uuid:id_shift>/', views.gerir_shift),
     path('', views.listar_shifts),
-    path('registar', views.registar_shift),
-    path('taxis-disponiveis', views.taxis_disponiveis),
-    path('driver/<int:driver_id>', views.listar_shifts_driver),
-    path('<uuid:id_shift>', views.gerir_shift),
 ]
