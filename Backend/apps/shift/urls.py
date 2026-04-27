de9fea154939d07eb50e338cb9b9aa6e37d09530
AfonsoSantos8 <afonso.santos172004@gmail.com>
2026-04-27 19:31:48 +0100

    merge conflits

--- Backend/apps/shift/urls.py
+++ Backend/apps/shift/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('taxis-disponiveis/', views.taxis_disponiveis),
-    path('registar/', views.registar_shift),
+    path('registar', views.registar_shift),
     path('driver/<int:driver_id>/', views.listar_shifts_driver),
     path('<uuid:id_shift>/', views.gerir_shift),
     path('', views.listar_shifts),
