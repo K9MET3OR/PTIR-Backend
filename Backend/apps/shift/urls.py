3f56882449c2db05d79da3958dcacfcaa2cfc394
Francisco Gomes <francisco.gomes.030505@gmail.com>
2026-05-04 14:46:19 +0100

    fix pagamentos

--- Backend/apps/shift/urls.py
+++ Backend/apps/shift/urls.py
@@ -3,9 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_shifts),
-    path('register', views.registar_shift),
-    path('taxis-available', views.taxis_disponiveis),
-    path('driver/<uuid:driver_id>', views.listar_shifts_driver),
-    path('<uuid:id_shift>/finish', views.terminar_shift),
-    path('<uuid:id_shift>', views.gerir_shift),
+    path('registar/', views.registar_shift),
+    path('taxis-disponiveis/', views.taxis_disponiveis),
+    path('driver/<uuid:driver_id>/', views.listar_shifts_driver),
+    path('<uuid:id_shift>/terminar/', views.terminar_shift),
+    path('<uuid:id_shift>/', views.gerir_shift),
 ]
