5a81b92156fc0eb688e2f6d4e044a8f112a11867
Apollo0010 <goncaloapolinario2@gmail.com>
2026-05-04 10:44:00 +0100

    Pagamento viagem 1.0

--- Backend/apps/trip/urls.py
+++ Backend/apps/trip/urls.py
@@ -4,11 +4,18 @@ from . import views
 
 urlpatterns = [
     path('', views.listar_trips),
+    path('register/', views.registar_trip),
     path('register', views.registar_trip),
+    path('<uuid:id_trip>/', views.gerir_trip),
     path('<uuid:id_trip>', views.gerir_trip),
+    path('<uuid:pk>/accept/', views.accept_trip),
     path('<uuid:pk>/accept', views.accept_trip),
+    path('<uuid:pk>/reject/', views.reject_trip),
     path('<uuid:pk>/reject', views.reject_trip),
+    path('<uuid:pk>/finish/', views.finish_trip),
     path('<uuid:pk>/finish', views.finish_trip),
+    path('pagamento/create/', views.criar_pagamento),
     path('pagamento/create', views.criar_pagamento),
+    path('pagamento/confirm/', views.confirmar_pagamento),
     path('pagamento/confirm', views.confirmar_pagamento),
 ]
